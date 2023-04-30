66d426573fe5e552faec12ff7948ab3caf7c2114
Alexander Panov <panov@royalzsoftware.de>
2023-04-30 15:54:46 +0200

    fix: invalid value

--- app/bid.rb
+++ app/bid.rb
@@ -3,7 +3,7 @@ class Bid
 	attr_reader :value
 
 	def initialize(value)
-		allowed_values = [1, 3, 5, 8, 13, 20]
+		allowed_values = [0, 1, 2, 3, 5, 8, 13, 20]
 		raise "Invalid value" unless allowed_values.include? value.to_i
 		@value = value
 	end
